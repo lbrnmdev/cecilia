9fbd0da1650d3873f50b6f924a948e43cc964691
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2019-02-02 02:38:57 +0100

    style flash

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,19 @@
 $(document).on('turbolinks:load', function() {
-    // your stuff here
+    // enable semanti ui dropdown
     $('.ui.dropdown')
       .dropdown()
+    ;
+    // enable semantic ui message dismissal
+    $('.message .close')
+      .on('click', function() {
+        $(this)
+          .closest('.ui.flash.container')
+          .transition('scale')
+        ;
+      })
+    ;
+    // make flash messages draw attention to themselves
+    $('.flash.message')
+      .transition('pulse')
     ;
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def semantic_ui_class_for(flash_type)
+    {
+        success: "positive",
+        error: "error",
+        alert: "error",
+        warning: "warning",
+        info: "info",
+        notice: "info"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
