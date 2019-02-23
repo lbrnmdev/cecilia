f8334ea2a44667005093eb07b593c40d89d92a1e
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2019-02-23 04:46:01 +0100

    fix associated membership not being created on user creating a workspace

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,6 @@
 class Membership < ApplicationRecord
-  belongs_to :workspace
-  belongs_to :user
+  belongs_to :workspace, inverse_of: :memberships
+  belongs_to :user, inverse_of: :memberships
 
   # validates_presence_of :workspace, :user # TODO: might not need as rails 5 defaults to validating presence of foreign key...
   validates_uniqueness_of :workspace_id, :scope => :user_id
