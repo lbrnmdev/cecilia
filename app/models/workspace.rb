46a807d5f67cf1ceb9103bff73a9560ee64eac73
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2019-02-27 01:38:14 +0100

    fix workspace user relationship to allow use of appropriate methods on workspace object in relation to users

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,7 +1,7 @@
 class Workspace < ApplicationRecord
   has_secure_token :sign_up_code
   has_many :memberships
-  has_many :workspaces, :through => :memberships
+  has_many :users, :through => :memberships
 
   validates :name, presence: true
 end
