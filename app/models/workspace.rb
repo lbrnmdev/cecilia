81737d11de4da56da0ebb22b3c5d23144cbab0c1
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2019-04-30 01:47:06 +0200

    generate and migrate wallet model

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -3,6 +3,7 @@ class Workspace < ApplicationRecord
   has_many :memberships, dependent: :destroy # TODO: maybe add before/after_destroy sending message to users saying workspace deleted
   has_many :users, :through => :memberships
   has_many :current_user_profiles, class_name: "UserProfile", foreign_key: "current_workspace_id", dependent: :nullify
+  has_many :wallets, dependent: :nullify
 
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_162222) do
+ActiveRecord::Schema.define(version: 2019_04_28_204459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 2019_02_28_162222) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wallets", force: :cascade do |t|
+    t.string "name"
+    t.bigint "workspace_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["workspace_id"], name: "index_wallets_on_workspace_id"
+  end
+
   create_table "workspaces", force: :cascade do |t|
     t.string "name", null: false
     t.string "sign_up_code"
@@ -65,4 +73,5 @@ ActiveRecord::Schema.define(version: 2019_02_28_162222) do
   add_foreign_key "memberships", "workspaces"
   add_foreign_key "user_profiles", "users"
   add_foreign_key "user_profiles", "workspaces", column: "current_workspace_id", on_delete: :nullify
+  add_foreign_key "wallets", "workspaces"
 end
