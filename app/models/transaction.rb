a2233cb06fd7a0ba40789fd18ce2102cfaa77740
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2019-06-06 03:38:13 +0200

    display transactions in table on disbursement show page

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,4 +4,14 @@ class Transaction < ApplicationRecord
   validates :msisdn, presence: true
   validates :no_of_attempts, numericality: {greater_than_or_equal_to: 0}
   validates :status, presence: true
+
+  # compute transaction fee (at 0.5%) # TODO: should be set somewhere else
+  def fee
+    (amount * 0.005) if amount
+  end
+
+  # compute transaction total
+  def total
+    (amount + fee) if amount
+  end
 end
