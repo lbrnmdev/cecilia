76a9a4c22437d7eebacbfd3ffb29c3dd788545e5
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2019-08-11 03:30:47 +0200

    adjust config vars for actioncable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = ENV.fetch('ACTION_CABLE_URL')
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ ENV.fetch('HTTPS_APP_URL'), ENV.fetch('HTTP_APP_URL') ]
+  config.web_socket_server_url = ENV.fetch('ACTION_CABLE_URL')
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
